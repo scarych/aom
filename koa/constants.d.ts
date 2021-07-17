0226b6c7e44993a638188c200ec93deec096a7f8
Grigory Kholstinnikov <holstinnikov@sharkdg.com>
2021-07-17 22:42:50 +0300

    add constants.d.ts

---
+++ koa/constants.d.ts
@@ -0,0 +1,13 @@
+export declare const BRIDGE_METADATA = "aom.koa:bridge";
+export declare const MARKERS_METADATA = "aom.koa:marker";
+export declare const MIDDLEWARE_METADATA = "aom.koa:middleware";
+export declare const MIDDLEWARES_LIST_METADATA = "aom.koa:middlewares_storage";
+export declare const IS_MIDDLEWARE_METADATA = "aom.koa:is_middleware";
+export declare const ENDPOINTS_METADATA = "aom.koa:endpoints";
+export declare const PARAMETERS_METADATA = "aom.koa:parameters";
+export declare const REVERSE_METADATA = "aom.koa:reverse_link";
+export declare const ERROR_METADATA = "aom.koa:error_result";
+export declare const CONSTRUCTOR_TYPE_ERROR =
+  "Wrong target type! The decorator allowed for static methods and class only";
+export declare const IS_MIDDLEWARE_ERROR = "Wrong middleware! The handler isn't marked as middleware";
+export declare const PARAMETER_HANDLER_ERROR = "Wrong handler for parameter decorator!";
