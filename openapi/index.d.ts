e92e3c325de2c4fb0a70ab9f2d54dc1308c08d33
Grigory Kholstinnikov <holstinnikov@sharkdg.com>
2021-07-24 11:42:02 +0300

    add OpenApi index.d.ts

---
+++ openapi/index.d.ts
@@ -0,0 +1,10 @@
+export declare class OpenApi {
+  Data(data: any): OpenApi;
+  Schemas(schemas: any): OpenApi;
+  Summary(summary: string): MethodDecorator;
+  Description(summary: string): MethodDecorator;
+  // Parameters(properties: any): MethodDecorator;
+  // Responses(codes: any): MethodDecorator;
+  // RequestBody(properties: any): MethodDecorator;
+  // Parameters(properties: any): MethodDecorator;
+}
