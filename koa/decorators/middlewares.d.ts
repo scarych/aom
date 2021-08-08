5585910f7d62039194b36870d8934aee9384fcf2
Grigory Kholstinnikov <holstinnikov@sharkdg.com>
2021-08-08 13:57:11 +0300

    extend marker handler info

--- koa/decorators/middlewares.d.ts
+++ koa/decorators/middlewares.d.ts
@@ -1,10 +1,15 @@
+import { $Cursor, $Target } from "./parameters";
+
 type CombinedDecorator = <T extends Function>(
   target: T,
   propertyKey?: string,
   descriptor?: any
 ) => void;
+
+type markerHandler = (target: $Target, cursor: $Cursor) => void | unknown;
+
 export declare function Use(...handlers: Function[]): CombinedDecorator;
 export declare function Middleware(): MethodDecorator;
 export declare function Bridge(prefix: string, nextRoute: Function): CombinedDecorator;
-export declare function Marker(handler): MethodDecorator;
+export declare function Marker(handler: markerHandler): MethodDecorator;
 export declare function Sticker(): MethodDecorator;
